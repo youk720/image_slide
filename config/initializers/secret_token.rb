9bdf1b2ea2d1f7f5fdf0a3bc1a13ef30b5931f8f
youk233_231 <yuuki_16n1100449@nnn.ed.jp>
2018-05-22 02:29:26 +0000

    github投稿

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Picmap::Application.config.secret_key_base = '9a025bddfc210ca6c54418cb77fdeed1849523963cad98bbb0ec90b985155f8ae6419d4040a654c07a0c018825f895b94ec7d8fede90394270e8e0c402790535'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Picmap::Application.config.session_store :cookie_store, key: '_picmap_session'
